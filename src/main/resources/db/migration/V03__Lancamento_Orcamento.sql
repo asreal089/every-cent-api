c4e244dfa8526d6ff6705ba04d2753bb3e45d939
asreal089 <lexx_89@hotmail.com>
2021-10-08 20:56:34 -0300

    add migration com lancamentos e orcamento

---
+++ src/main/resources/db/migration/V03__Lancamento_Orcamento.sql
@@ -0,0 +1,19 @@
+CREATE TABLE lancamento(
+	user_id bigint,
+	tipo_id int,
+	descricao varchar(50),
+	data_lancamento date,
+	valor numeric(7,2),
+	FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+	FOREIGN KEY (tipo_id) REFERENCES tipo_lancamento(tipo_id),
+	PRIMARY KEY (user_id, tipo_id)
+);
+
+create table orcamento(
+	user_id bigint,
+	tipo_id int,
+	valor_limite numeric(7,2),
+	FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+	FOREIGN KEY (tipo_id) REFERENCES tipo_lancamento(tipo_id),
+	PRIMARY KEY (user_id, tipo_id)	
+);
