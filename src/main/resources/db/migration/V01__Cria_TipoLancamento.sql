c29aa6bea32a25e69b24867d97d34d985ecf004f
asreal089 <lexx_89@hotmail.com>
2023-03-16 06:44:30 -0300

    Ajuste no tipo de gasto.

--- src/main/resources/db/migration/V01__Cria_TipoLancamento.sql
+++ src/main/resources/db/migration/V01__Cria_TipoLancamento.sql
@@ -4,17 +4,17 @@ CREATE TABLE tipo_lancamento(
 	is_renda boolean DEFAULT FALSE
 );
 
-INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('salario', true);
-INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('proventos', true);
-INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('outras rendas', true);
+INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('Salário', true);
+INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('Proventos', true);
+INSERT INTO tipo_lancamento (tipo_nome, is_renda) VALUES ('Outras rendas', true);
 INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Moradia');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('locomoção');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('comer fora');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('shopping');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('seguro');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('cartão de credito');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('plano de saúde');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('educação');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('mercado');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('lazer');
-INSERT INTO tipo_lancamento (tipo_nome) VALUES ('outros');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Transporte');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Alimentação');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Utilidades');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Saúde');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Educação');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Roupas');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Lazer Entreterimento');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Seguro');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Investimentos');
+INSERT INTO tipo_lancamento (tipo_nome) VALUES ('Outros');
